25df91482ee231cee458864cd6b07d6512973d3a
Jhonny Lanzuisi <jalb97@gmail.com>
2023-05-06 12:03:40 -0400

    Solve day7 in go

--- go/day7/aocday7.go
+++ go/day7/aocday7.go
@@ -1,4 +1,4 @@
-package aocday7
+package main
 
 import (
 	"bufio"
@@ -6,25 +6,85 @@ import (
 	"golang.org/x/exp/slices"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
+func main() {
+	filelist, dirlist := parse_input()
+	fmt.Printf("Part 1: %v\n", part1(filelist, dirlist))
+	fmt.Printf("Part 2: %v\n", part2(filelist, dirlist))
+}
+
 type file struct {
 	name    string
 	size    int
-	parents []string
+	parents string
 }
 
-func aocday7() {
-	filelist, dirlist := parse_input()
+func part2(f []file, d []string) int {
+	const disksize = 70000000
+	const targetsize = 30000000
+	const root = "/root/"
+	spaceneeded := 0
+	spaceunused := 0
+	rootsize := 0
+	dirsize := 0
+	var candidates []int
 
-	fmt.Println(filelist)
-	fmt.Println(dirlist)
+	for _, file := range f {
+		if strings.Contains(file.parents, root) {
+			rootsize += file.size
+		}
+	}
+
+	spaceunused = disksize - rootsize
+	spaceneeded = targetsize - spaceunused
+
+	for _, dir := range d {
+		dirsize = 0
+		for _, file := range f {
+			if strings.Contains(file.parents, dir) {
+				dirsize += file.size
+			}
+		}
+		if dirsize >= spaceneeded {
+			candidates = append(candidates, dirsize)
+		}
+	}
+
+	sort.Ints(candidates)
+
+	return candidates[0]
+}
+
+func part1(f []file, d []string) int {
+	const upperbound = 100000
+	dirsize := 0
+	result := 0
+
+	for _, dir := range d {
+		dirsize = 0
+		for _, file := range f {
+			if strings.Contains(file.parents, dir) {
+				dirsize += file.size
+			}
+		}
+		if dirsize < upperbound {
+			result += dirsize
+		}
+	}
+
+	//fmt.Println(dirlist)
+	//fmt.Println(filelist)
+	//fmt.Println(result)
+
+	return result
 }
 
 func parse_input() ([]file, []string) {
-	input, err := os.Open("example.txt")
+	input, err := os.Open("input.txt")
 
 	if err != nil {
 		log.Fatal(err)
@@ -37,25 +97,35 @@ func parse_input() ([]file, []string) {
 	var parentlist []string
 	var filelist []file
 	var dirlist []string
+	const root = "/root/"
 
 	for inputscan.Scan() {
 		line = strings.Split(inputscan.Text(), " ")
+		fullpath := ""
 		if line[0] == "$" { // its a command
 			if line[len(line)-1] == ".." {
 				parentlist = parentlist[:len(parentlist)-1]
 			} else {
 				if line[1] == "cd" {
-					parentlist = append(parentlist, line[2])
-
+					if line[2] == "/" {
+						parentlist = append(parentlist, root)
+					} else {
+						parentlist = append(parentlist, line[2]+"/")
+					}
+					for _, s := range parentlist {
+						fullpath += s
+					}
+					if !slices.Contains(dirlist, fullpath) {
+						dirlist = append(dirlist, fullpath)
+					}
 				}
 			}
 		} else if line[0] != "dir" {
 			size, _ := strconv.Atoi(line[0])
-			filelist = append(filelist, file{
-				name:    line[1],
-				size:    size,
-				parents: parentlist,
-			})
+			for _, s := range parentlist {
+				fullpath += s
+			}
+			filelist = append(filelist, file{line[1], size, fullpath})
 		}
 	}
 
---
+++ go/day7/go.mod
@@ -0,0 +1,5 @@
+module github.com/JLanzuisi/aoc-2022/tree/main/go/day7
+
+go 1.20
+
+require golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53 // indirect
